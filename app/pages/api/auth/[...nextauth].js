8bb50d93e27a9f61d96039ac3abba24a20c72073
Eric Elliott <ercxyz@gmail.com>
2020-09-09 11:23:45 -0400

    still trying to get nextauth to work

--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -3,16 +3,16 @@ import Providers from "next-auth/providers"
 
 const providers = [
 	Providers.Twitter({
-		clientId     : process.env.TWITTER_ID,
-		clientSecret : process.env.TWITTER_SECRET,
+		clientId: process.env.TWITTER_ID,
+		clientSecret: process.env.TWITTER_SECRET,
 	}),
 ]
 
 const callbacks = {}
 callbacks.redirect = async (url, baseUrl) => {
-	console.log(url)
-	console.log(baseUrl)
-	return Promise.resolve(baseUrl + "/api/auth/callback")
+	console.log("redirect url:", url)
+	console.log("base url:", baseUrl)
+	return Promise.resolve(baseUrl.replace("localhost", "127.0.0.1"))
 }
 
 const options = {
